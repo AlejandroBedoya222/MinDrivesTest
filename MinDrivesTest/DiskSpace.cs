8acc9e21135465748549938ac863ecd121f91350
AlejandroBedoya222 <ytolopez222@gmail.com>
2019-12-24 03:26:31 +0100

    Updated MinDrive and MinDriveLinq

--- MinDrivesTest/DiskSpace.cs
+++ MinDrivesTest/DiskSpace.cs
@@ -11,110 +11,56 @@ namespace MinDrivesTest
     public class DiskSpace
     {
         /// <summary>
-        /// The LINQ version is built in using the latest techniques and the power of lambda expressions,this version 
-        /// also uses a collection(List) of class “HardDrive” as a helper to easily store the Used and Total properties
-        /// within the same container for further uses instead of having to handle two separated arrays.
+        /// The LINQ version is built in using the latest techniques and the power of lambda expressions
         /// </summary>
         /// <param name="used"></param>
         /// <param name="total"></param>
         /// <returns>Return the minimum number of hard drives needed to store the data after the consolidation is complete</returns>
         public static int MinDrivesLinq(int[] used, int[] total)
         {
-            if (used.Length == 1)
+            int totalUsed = used.Sum();
+            int count = 0;
+
+            var sortedListDisk = total.OrderByDescending(x => x).ToList();
+            while (totalUsed > 0)
             {
-                return 1;
+                totalUsed -= sortedListDisk.ElementAt(count);
+                count++;
             }
-            var sortedHardDrives = total.Select((t, i) => new HardDrive { Used = used[i], Total = t })
-                                        .OrderByDescending(t => t.Total).ToList();
-
-            var hardDrives = sortedHardDrives.Select((hd, i) =>
-            {
-                var available = hd.Total - hd.Used;
-                var index = i;
-                while (available > 0 && index < sortedHardDrives.Count() - 1)
-                {
-                    var nextElement = sortedHardDrives.ElementAt(index + 1);
-                    if (available > nextElement.Used)
-                    {
-                        hd.Used = hd.Used + nextElement.Used;
-                        nextElement.Used = 0;
-                    }
-                    else
-                    {
-                        //hd.Used = hd.Used + available;
-                        hd.Used = hd.Used + nextElement.Used - (nextElement.Used - available);
-                        nextElement.Used = nextElement.Used - available;
-                    }
-                    available = hd.Total - hd.Used;
-
-                    index++;
-                }
-                return hd;
-            }).ToList();
-
-            return hardDrives.Where(hd => hd.Used > 0).Count();
+            return count;
         }
 
         /// <summary>
-        /// The “conventional” version is just a raw implementation using the basic arrays and loops available on any languages.
+        /// The “” version is just a raw implementation using the basic arrays and loops available on any languages.
         /// </summary>
         /// <param name="used"></param>
         /// <param name="total"></param>
         /// <returns>Return the minimum number of hard drives needed to store the data after the consolidation is complete</returns>
-        public static int MinDrivesConventional(int[] used, int[] total)
+        public static int MinDrives(int[] used, int[] total)
         {
-            if (used.Length == 1)
+            int length = used.Length;
+            for (int i = 0; i < length - 1; i++)
             {
-                return 1;
-            }
-            //Sorting descending
-            for (int i = 0; i < total.Length - 1; i++)
-            {
-                for (int j = i + 1; j < total.Length; j++)
+                for (int j = i + 1; j < length; j++)
                 {
                     if (total[j] > total[i])
                     {
                         var temp = total[j];
                         total[j] = total[i];
                         total[i] = temp;
-
-                        temp = used[j];
-                        used[j] = used[i];
-                        used[i] = temp;
                     }
                 }
             }
-
-            //Moving the data
-            for (int i = 0; i < total.Length; i++)
+            int totalUsed = 0;
+            foreach (var item in used)
             {
-                var available = total[i] - used[i];
-                var index = i;
-                while (available > 0 && index < total.Length - 1)
-                {
-                    if (available > used[index + 1])
-                    {
-                        used[i] = used[i] + used[index + 1];
-                        used[index + 1] = 0;
-                    }
-                    else
-                    {
-                        used[i] = used[i] + used[index + 1] - (used[index + 1] - available);
-                        used[index + 1] = used[index + 1] - available;
-                    }
-                    available = total[i] - used[i];
-                    index++;
-                }
+                totalUsed += item;
             }
-
-            //Counting the in use ones
-            var count = 0;
-            for (int i = 0; i < used.Length; i++)
+            int count = 0;
+            while (totalUsed > 0)
             {
-                if (used[i] > 0)
-                {
-                    count++;
-                }
+                totalUsed -= total[count];
+                count++;
             }
             return count;
         }
--- MinDrivesTest_XUnitTest/DiskSpaceTest.cs
+++ MinDrivesTest_XUnitTest/DiskSpaceTest.cs
@@ -20,13 +20,13 @@ namespace MinDrivesTest_XUnitTest
         }
 
         [Fact]
-        public void MinDrivesConventionalresult5()
+        public void MinDrivesresult5()
         {
             //Arrange
             var used = new int[] { 750, 800, 850, 900, 950 };
             var total = new int[] { 800, 850, 900, 950, 1000 };
             //Act
-            var result = DiskSpace.MinDrivesConventional(used, total);
+            var result = DiskSpace.MinDrives(used, total);
             //Assert                             
             Assert.IsType<int>(result);
             Assert.True(result == 5);
@@ -46,13 +46,13 @@ namespace MinDrivesTest_XUnitTest
         }
 
         [Fact]
-        public void MinDrivesConventionalResult1()
+        public void MinDrivesResult1()
         {
             //Arrange
             var used = new int[] { 1, 200, 200, 199, 200, 200 };
             var total = new int[] { 1000, 200, 200, 200, 200, 200 };
             //Act
-            var result = DiskSpace.MinDrivesConventional(used, total);
+            var result = DiskSpace.MinDrives(used, total);
             //Asserts
             Assert.IsType<int>(result);
             Assert.Equal(1, result);
@@ -72,13 +72,13 @@ namespace MinDrivesTest_XUnitTest
         }
 
         [Fact]
-        public void MinDrivesConventionalResult49()
+        public void MinDrivesResult49()
         {
             //Arrange
             var used = new int[] { 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49 };
             var total = new int[] { 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50 };
             //Act
-            var result = DiskSpace.MinDrivesConventional(used, total);
+            var result = DiskSpace.MinDrives(used, total);
             //Asserts
             Assert.IsType<int>(result);
             Assert.Equal(49, result);
